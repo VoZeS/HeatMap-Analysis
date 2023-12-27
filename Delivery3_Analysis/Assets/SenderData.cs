89d203b2b59662ae6e997084f184c3a2f971294a
Erik Martín Garzón <erikmarting1200@gmail.com>
2023-12-27 17:12:53 +0100

    basic implementations + gerard's work

--- Delivery3_Analysis/Assets/SenderData.cs
+++ Delivery3_Analysis/Assets/SenderData.cs
@@ -21,22 +21,29 @@ public class SenderData : MonoBehaviour
 
     private void OnEnable()
     {
-        // ------------------------- WORK IN PROGRESS
 
-        //damageableScript.OnDeath += SendKillData;
-        //Simulator.OnNewSession += SendNewSessionTime;
-        //Simulator.OnEndSession += SendEndSessionTime;
-        //Simulator.OnBuyItem += SendbuyInfo;
+        damageableScript.OnDeath.AddListener(SendKillData);
+
+        //damageableScript.OnReceiveDamage.AddListener(func);
+        //damageableScript.OnHitWhileInvulnerable.AddListener(func);
+        //damageableScript.OnBecomeVulnerable.AddListener(func);
+        //damageableScript.OnResetDamage.AddListener(func);
+
+       
+
 
     }
     private void OnDisable()
     {
-        // ------------------------- WORK IN PROGRESS
 
-        //Simulator.OnNewPlayer -= SendData;
-        //Simulator.OnNewSession -= SendNewSessionTime;
-        //Simulator.OnEndSession -= SendEndSessionTime;
-        //Simulator.OnBuyItem -= SendbuyInfo;
+        damageableScript.OnDeath.RemoveListener(SendKillData);
+
+        //damageableScript.OnReceiveDamage.RemoveListener(func);
+        //damageableScript.OnHitWhileInvulnerable.RemoveListener(func);
+        //damageableScript.OnBecomeVulnerable.RemoveListener(func);
+        //damageableScript.OnResetDamage.RemoveListener(func);
+
+
 
     }
 
@@ -109,30 +116,127 @@ public class SenderData : MonoBehaviour
         // -------------------------------------------------------------------------------------------------------------------- SEND HEATMAP DEATH DATA
         public void SendDeathData(int sessionID, int runID, Transform playerPosDeath, Transform enemyPosKill, DateTime time)
     {
-        // ------------------------- WORK IN PROGRESS
+        // ------------------------- WORK DONE BUT NECESSARY??
 
-        //StartCoroutine(SendPlayerDeathCoroutine(sessionID, runID, playerPosDeath, enemyPosKill, time));
+        StartCoroutine(SendPlayerDeathCoroutine(sessionID, runID, playerPosDeath, enemyPosKill, time));
 
     }
-    //private IEnumerator SendPlayerDeathCoroutine(int sessionID, int runID, Transform playerPosDeath, Transform enemyPosKill, DateTime time)
-    //{
+    private IEnumerator SendPlayerDeathCoroutine(int sessionID, int runID, Transform playerPosDeath, Transform enemyPosKill, DateTime time)
+    {
+        
+        string formatoPersonalizado = "yyyy-MM-dd HH:mm:ss";
 
-    //}
+        string fechaFormateada = time.ToString(formatoPersonalizado);
+
+        WWWForm formUser = new WWWForm();
+        formUser.AddField("SessionID", sessionID);
+        formUser.AddField("RunID", runID);
+        formUser.AddField("PlayerKiller_PositionX", ((int)playerPosDeath.transform.position.x)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("PlayerKiller_PositionY", ((int)playerPosDeath.transform.position.y)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("PlayerKiller_PositionZ", ((int)playerPosDeath.transform.position.z)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("EnemyDeath_PositionX", ((int)enemyPosKill.transform.position.x));
+        formUser.AddField("EnemyDeath_PositionY", ((int)enemyPosKill.transform.position.y));
+        formUser.AddField("EnemyDeath_PositionZ", ((int)enemyPosKill.transform.position.z));
+        formUser.AddField("Time", fechaFormateada);
+
+
+        UnityWebRequest www = UnityWebRequest.Post(serverURL, formUser);
+
+
+        yield return www.SendWebRequest();
+
+        // Verificar si hubo un error en la solicitud
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Datos DE LA DEATH enviados con exito al servidor.");
+            Debug.Log(www.downloadHandler.text);
+
+            string userId_String = www.downloadHandler.text;
+
+            if (uint.TryParse(userId_String, out userId_uInt))
+            {
+                // La conversión fue exitosa, y valorComoInt contiene el valor entero.
+                //CallbackEvents.OnAddPlayerCallback?.Invoke(userId_uInt);
+                Debug.Log(www.downloadHandler.text);
+            }
+            else
+            {
+                // La conversión falló, puedes manejar el error aquí.
+                Debug.Log("Error DEATHID");
+                Debug.Log(www.downloadHandler.text);
+            }
+
+        }
+        else
+        {
+            Debug.LogError("Error al enviar datos DE LA DEATH al servidor: " + www.error);
+        }
+
+
+
+
+    }
 
     // --------------------------------------------------------------------------------------------------------------------
 
     // -------------------------------------------------------------------------------------------------------------------- SEND HEATMAP PATH
     public void SendPathData(int sessionID, int runID, Transform playerPos, DateTime time)
     {
-        // ------------------------- WORK IN PROGRESS
+        // ------------------------- WORK DONE BUT NECESSARY??
 
-        //StartCoroutine(SendPlayerDeathCoroutine(sessionID, runID, playerPos, time));
+        StartCoroutine(SendPlayerDeathCoroutine(sessionID, runID, playerPos, time));
 
     }
-    //private IEnumerator SendPlayerDeathCoroutine(int sessionID, int runID, Transform playerPos, DateTime time)
-    //{
+    private IEnumerator SendPlayerDeathCoroutine(int sessionID, int runID, Transform playerPos, DateTime time)
+    {
+        string formatoPersonalizado = "yyyy-MM-dd HH:mm:ss";
 
-    //}
+        string fechaFormateada = time.ToString(formatoPersonalizado);
+
+        WWWForm formUser = new WWWForm();
+        formUser.AddField("SessionID", sessionID);
+        formUser.AddField("RunID", runID);
+        formUser.AddField("PlayerKiller_PositionX", ((int)playerPos.transform.position.x)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("PlayerKiller_PositionY", ((int)playerPos.transform.position.y)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("PlayerKiller_PositionZ", ((int)playerPos.transform.position.z)); //Revisar si el text entre cometes es correcte pel php
+        formUser.AddField("Time", fechaFormateada);
+
+
+        UnityWebRequest www = UnityWebRequest.Post(serverURL, formUser);
+
+
+        yield return www.SendWebRequest();
+
+        // Verificar si hubo un error en la solicitud
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Datos DEL PATH enviados con exito al servidor.");
+            Debug.Log(www.downloadHandler.text);
+
+            string userId_String = www.downloadHandler.text;
+
+            if (uint.TryParse(userId_String, out userId_uInt))
+            {
+                // La conversión fue exitosa, y valorComoInt contiene el valor entero.
+                //CallbackEvents.OnAddPlayerCallback?.Invoke(userId_uInt);
+                Debug.Log(www.downloadHandler.text);
+            }
+            else
+            {
+                // La conversión falló, puedes manejar el error aquí.
+                Debug.Log("Error PATHID");
+                Debug.Log(www.downloadHandler.text);
+            }
+
+        }
+        else
+        {
+            Debug.LogError("Error al enviar datos DEL PATH al servidor: " + www.error);
+        }
+
+
+
+    }
 }
 
 
