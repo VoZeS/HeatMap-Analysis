e9a4fadc17ac2f654741cd8e5d18d6f207357462
VoZeS <davidboces12@hotmail.com>
2024-01-07 19:14:27 +0100

    PathReader

---
+++ Database_PathReader.php
@@ -0,0 +1,36 @@
+<?php
+// Data base connection
+$server = "localhost";
+$user = "davidbo5";
+$password = "9kPnax8NCqjb";
+$dbname = "davidbo5";
+
+$connection = mysqli_connect($server, $user, $password, $dbname);
+
+if (!$connection) {
+  die("Error al conectar a la base de datos: " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    // HEAT MAP KILL DATA
+    $sql = "SELECT * FROM HeatMap_Path";
+
+    $result = mysqli_query($connection, $sql);
+
+    if ($result) {
+        $data = array();
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        // Convertir datos a formato JSON y enviarlos a Unity
+        echo json_encode($data);
+    } else {
+        echo "Error al ejecutar la consulta: " . mysqli_error($connection);
+    }
+}
+
+// CLOSE
+mysqli_close($connection);
+?>
--- Delivery3_Analysis/Assets/DataAnalysis/DatabaseReader.cs
+++ Delivery3_Analysis/Assets/DataAnalysis/DatabaseReader.cs
@@ -72,16 +72,52 @@ public class HeatMapDeathData
     }
 }
 
+[System.Serializable]
+public class PathData
+{
+    public int PathID;
+    public int SessionID;
+    public int RunID;
+
+    // Positions
+    public int Player_PositionX;
+    public int Player_PositionY;
+    public int Player_PositionZ;
+
+    public int Player_RotationX;
+    public int Player_RotationY;
+    public int Player_RotationZ;
+
+    // Vectors
+    public Vector3 playerPosition;
+    public Vector3 playerRotation;
+
+    public string Time;
+
+    public PathData(int path_id, int session_id, int run_id, int playerPositionX, int playerPositionY, int playerPositionZ,
+                           int playerRotationX, int playerRotationY, int playerRotationZ, string _time)
+    {
+        PathID = path_id;
+        SessionID = session_id;
+        RunID = run_id;
+        playerPosition = new Vector3(playerPositionX, playerPositionY, playerPositionZ);
+        playerRotation = new Vector3(playerRotationX, playerRotationY, playerRotationZ);
+        Time = _time;
+    }
+}
+
 public class DatabaseReader : MonoBehaviour
 {
     public List<HeatMapKillData> killDataList = new List<HeatMapKillData>();
     public List<HeatMapDeathData> deathDataList = new List<HeatMapDeathData>();
+    public List<PathData> pathDataList = new List<PathData>();
 
     void Start()
     {
         // Called in START, but can be called when NECESSARY
         StartCoroutine(ReadKillDataFromPHP());
         StartCoroutine(ReadDeathDataFromPHP());
+        StartCoroutine(ReadPathDataFromPHP());
     }
 
     IEnumerator ReadKillDataFromPHP()
@@ -158,6 +194,44 @@ public class DatabaseReader : MonoBehaviour
            // Debug.Log("Debug Example 2: " + deathDataList[57].DeathID + " " + deathDataList[57].playerDeathPosition);
            // Debug.Log("Debug Example 3: " + deathDataList[80].DeathID + " " + deathDataList[80].playerDeathPosition);
             //Debug.Log("Debug Example 4: " + deathDataList[10].DeathID + " " + deathDataList[10].playerDeathPosition);
+        }
+    }
+
+    IEnumerator ReadPathDataFromPHP()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://citmalumnes.upc.es/~davidbo5/Database_PathReader.php");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("Error al leer datos desde PHP: " + www.error);
+        }
+        else
+        {
+            // Parser JSON
+            string jsonString = www.downloadHandler.text;
+            Debug.Log(jsonString);
+
+            // Deserialize JSON to an array
+            PathData[] dataArray = JsonHelper.FromJson<PathData>(jsonString);
+
+            foreach (var data in dataArray)
+            {
+
+                PathData pathData = new PathData(data.PathID, data.SessionID, data.RunID,
+               data.Player_PositionX, data.Player_PositionY, data.Player_PositionZ,
+               data.Player_RotationX, data.Player_RotationY, data.Player_RotationZ, data.Time);
+
+
+                pathDataList.Add(pathData);
+
+            }
+
+            // DEBUG EXAMPLE!
+            // Debug.Log("Debug Example 1: " + pathDataList[50].PathID + " " + pathDataList[50].playerPosition);
+            // Debug.Log("Debug Example 2: " + pathDataList[57].PathID + " " + pathDataList[57].playerPosition);
+            // Debug.Log("Debug Example 3: " + pathDataList[80].PathID + " " + pathDataList[80].playerPosition);
+            //Debug.Log("Debug Example 4: " + pathDataList[10].PathID + " " + pathDataList[10].playerPosition);
         }
     }
 
